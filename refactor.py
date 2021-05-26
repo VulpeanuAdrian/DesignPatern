aab6aa1c36cb9506386eee3689a5b5d8a459a297
Vulpeanu Adrian <vulpeanuadrian1994@gmail.com>
2021-05-26 22:15:54 +0200

    git commit

--- refactor.py
+++ refactor.py
@@ -2,7 +2,41 @@ refactor.py
 
 
 
+'''
+Problema de implementat
+ Create a console application that iterates through a list of shapes (at least three
+different shape types (e.g., triangle, circle, and rectangle)) and displays the following
+message: &quot;area of &lt;shape type&gt; is calculated using this &lt;formula&gt;&quot;.
 
+- Problema trebuie implementata intr-un limbaj OOP (C#, Java sau C++) si pusa pe source
+control; proiectul trebuie sa fie compilabil
+- Problema trebuie sa aiba o implementare initiala pana in 1 Iunie; nerespectarea termenului
+va duce la injumatatirea punctelor care pot fi primite pentru acest proiect
+- Problema trebuie sa aiba cel putin o iteratie de refactoring (pana la data examenului)
+'''
+
+import collections
+
+Shapes = collections.namedtuple('Shape', ['figure', 'formula'])
+
+class Shapes:
+
+    def __init__(self):
+        figure=["circle","triangle","cube",""
+        formula='A = π r² , A = 1/2 × b × h , 6a2'.split(",")
+    self._shapes = [Shapes(figure, formula) for shape in self.figure
+                       for rank in self.formula]
+
+    def __len__(self):
+        return len(self._shapes)
+
+    def __getitem__(self, position):
+        return self._shapes[position]
+
+
+m=Shapes()
+
+print(len(m))
 
 
 import time
