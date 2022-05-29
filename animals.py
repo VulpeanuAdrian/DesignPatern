6ba79f0ef80374f4e56f7e9aebcee58e33d742f6
Vulpeanu Adrian <vulpeanuadrian1994@gmail.com>
2022-05-29 18:19:39 +0100

    Refactor after review made by Valentin ogrean-> create Zoo class in order to track all animals, also use Animal as a base class.

--- animals.py
+++ animals.py
@@ -1,29 +1,41 @@
-class Dog:
-    def make_sound(self):
-        return f"{self.__class__.__name__} is barking !"
-
-
-class Pig:
-
-    def make_sound(self):
-        return f"{self.__class__.__name__} is quizquiz !"
-
-
-class Cat:
-    def make_sound(self):
-        return f"{self.__class__.__name__} is meow !"
-
-
 class Animal:
+    sound = ""
 
+    def __init__(self, name):
+        self.name = name
+
+    def set_category(self, sound):
+        self.sound = sound
+
+
+class Pig(Animal):
+    sound = 'quizquiz'
+
+
+class Cat(Animal):
+    sound='meow meow'
+
+class Dog(Animal):
+    sound='bark bark'
+
+class Zoo:
     def __init__(self):
-        self.animal_list = [Dog(), Cat(), Pig()]
+        self.current_animals = {}
 
-    def get_animal_sounds(self):
-        for animal in self.animal_list:
-            print(animal.make_sound())
+    def add_animal(self, animal):
+        self.current_animals[animal.name] = animal.sound
+
+    def print_animals_sounds(self):
+        for animal in self.current_animals:
+            print(f"{animal} is making the sound {self.current_animals[animal]}")
 
 
 if __name__ == '__main__':
-    animals = Animal()
-    animals.get_animal_sounds()
+    zoo = Zoo()
+    dog = Dog("Chuwuauwa")
+    cat = Cat("Persian")
+    pig = Pig ('Scrof')
+    zoo.add_animal(dog)
+    zoo.add_animal(cat)
+    zoo.add_animal(pig)
+    zoo.print_animals_sounds()
